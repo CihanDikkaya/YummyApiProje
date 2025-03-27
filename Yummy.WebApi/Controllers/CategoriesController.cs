2c895b78b4e27ec8e38737166166895ca01f877b
CihanDikkaya <cihandikkaya09@hotmail.com>
2025-03-27 16:34:46 +0300

    Kategori Tablosu için APİ işlemleri tamamlandı.

---
+++ Yummy.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using Yummy.WebApi.Context;
+using Yummy.WebApi.Entity;
+
+namespace Yummy.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori Eklendi...");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var values = _context.Categories.Find(id);
+            _context.Categories.Remove(values);
+            _context.SaveChanges();
+            return Ok("Kategori Silindi...");
+        }
+
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var values = _context.Categories.Find(id);
+            return Ok(values);
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori Güncellendi...");
+        }
+    }
+}
