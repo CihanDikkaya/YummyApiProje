29e8368878c2b08edfbcb46dbec7a28c64401e87
CihanDikkaya <cihandikkaya09@hotmail.com>
2025-04-26 13:20:33 +0300

    El ile Contact tablosuna veri girişi.

---
+++ Yummy.WebApi/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yummy.WebApi.Context;
+using Yummy.WebApi.DTOS.ContactDTO;
+using Yummy.WebApi.Entity;
+
+namespace Yummy.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ContactController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ContactList()
+        {
+            var val = _context.Contacts.ToList();
+            return Ok(val);
+        }
+        public IActionResult CreateContact(CreateContactDTO createContactDTO)
+        {
+            Contact contact = new Contact();
+            contact.Email = createContactDTO.Email;
+            contact.Address = createContactDTO.Address;
+            contact.Phone = createContactDTO.Phone;
+            contact.Location = createContactDTO.Location;
+            contact.OpenHours = createContactDTO.OpenHours;
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+            return Ok("Ekleme İşlemi Başarılı");
+        }
+    }
---
+++ Yummy.WebApi/DTOS/ContactDTO/UpdateContactDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Yummy.WebApi.DTOS.ContactDTO
+{
+    public class UpdateContactDTO
+    {
+        public int ContactID { get; set; }
+        public string Location { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string OpenHours { get; set; }
+    }
+}
